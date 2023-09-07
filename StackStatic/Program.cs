276f08f84ff78bf3b6eda58b5ed0d3a489bd1de4
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-09-06 23:09:44 -0600

    Agregar archivos de proyecto.

---
+++ StackStatic/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackStatic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StackExample pila = new StackExample(5);
+            bool salir = true;
+
+            while (salir)
+            {
+                Console.WriteLine("// Select an option" +
+                    "\n[1]: Enter a value " +
+                    "\n[2]: Delete a value" +
+                    "\n[3]: Show stack" +
+                    "\n[4]: Exit");
+
+                int opcion;
+
+                try
+                {
+                    opcion = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                switch (opcion)
+                {
+
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Enter a value:");
+                        pila.Push(Convert.ToInt32(Console.ReadLine()));
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        if (!pila.IsEmpty())
+                        {
+                            int elementoDesapilado = pila.Pop();
+                            Console.Clear();
+                            Console.WriteLine("// Element popped from the stack: " + elementoDesapilado);
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("The stack is empty");
+                        }
+
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        pila.DisplayStack();
+                        break;
+
+                    case 4:
+                        salir = false;
+                        Console.Clear();
+                        Console.WriteLine("Good bye");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ StackStatic/StackExample.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackStatic
+{
+    internal class StackExample
+    {
+        private int maxSize; // Tamaño máximo de la pila
+        private int top;     // Índice del elemento superior
+        private int[] stack; // Arreglo para almacenar elementos
+
+        public StackExample(int size)
+        {
+            maxSize = size;
+            top = -1;
+            stack = new int[maxSize];
+        }
+
+        public void Push(int item)
+        {
+            if (top < maxSize - 1)
+            {
+                stack[++top] = item;
+            }
+            else
+            {
+                Console.WriteLine("La pila está llena. No se puede agregar más elementos.");
+            }
+        }
+
+        public int Pop()
+        {
+            if (top >= 0)
+            {
+                return stack[top--];
+            }
+            else
+            {
+                Console.WriteLine("La pila está vacía. No se puede sacar ningún elemento.");
+                return -1; // Valor de retorno predeterminado para indicar un error
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        public void DisplayStack()
+        {
+            if (!IsEmpty())
+            {
+                Console.WriteLine("Stack elements:");
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The stack is empty");
+            }
+        }
+    }
+}
